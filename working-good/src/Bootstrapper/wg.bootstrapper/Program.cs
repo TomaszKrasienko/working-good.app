3a1b2832c9193696daa3c5ac00658b2a0a455cb4
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-03-06 20:19:35 +0100

    Added companies domain project

--- working-good/src/Bootstrapper/wg.bootstrapper/Program.cs
+++ working-good/src/Bootstrapper/wg.bootstrapper/Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Host.ConfigureModules();
 builder.Services.AddHealthChecks();
-
 var assemblies = ModuleLoader.GetAssemblies(builder.Configuration);
 var modules = ModuleLoader.GetModules(assemblies);
 builder.Services.AddModulesConfiguration(modules);
