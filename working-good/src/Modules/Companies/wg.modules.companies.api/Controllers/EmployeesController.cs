949074390da977f4477d1a55eb1adee940c8824a
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-03-13 19:36:19 +0100

    Added employee controller with add employee endpoint. Added response type to add company endpoint

--- working-good/src/Modules/Companies/wg.modules.companies.api/Controllers/EmployeesController.cs
+++ working-good/src/Modules/Companies/wg.modules.companies.api/Controllers/EmployeesController.cs
@@ -1,35 +1,33 @@
-using System.Net;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using wg.modules.companies.application.CQRS.Companies.AddCompany;
+using wg.modules.companies.application.CQRS.Employees.AddEmployee;
 using wg.shared.abstractions.CQRS.Commands;
 using wg.shared.abstractions.CQRS.Queries;
 
 namespace wg.modules.companies.api.Controllers;
 
-internal sealed class CompaniesController(
+internal sealed class EmployeesController(
     ICommandDispatcher commandDispatcher, 
     IQueryDispatcher queryDispatcher) : BaseController
 {
     [HttpGet("{id:guid}")]
-    [Authorize]
     public async Task<ActionResult> GetById(Guid id)
     {
         throw new NotImplementedException();
-    }
+    } 
     
+    [HttpPost("companies/{companyId}/add")]
     [Authorize(Roles = "Manager")]
-    [HttpPost("add")]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult> AddCompany(AddCompanyCommand command, CancellationToken cancellationToken)
+    public async Task<ActionResult> AddEmployee(Guid companyId, AddEmployeeCommand command, CancellationToken cancellationToken)
     {
-        var companyId = Guid.NewGuid();
-        await commandDispatcher.SendAsync(command with { Id = companyId }, cancellationToken);
-        AddResourceHeader(companyId);
+        var employeeId = Guid.NewGuid();
+        await commandDispatcher.SendAsync(command with { CompanyId = companyId, Id = employeeId }, cancellationToken);
+        AddResourceHeader(employeeId);
         return CreatedAtAction(nameof(GetById), new { id = companyId }, null);
     }
 }
