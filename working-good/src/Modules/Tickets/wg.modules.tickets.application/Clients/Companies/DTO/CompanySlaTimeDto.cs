cb2182bb3ee0824f52d4fe4bfd83d1f90c29ad14
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-03-18 20:59:04 +0100

    Added api client interface with DTO

---
+++ working-good/src/Modules/Tickets/wg.modules.tickets.application/Clients/Companies/DTO/CompanySlaTimeDto.cs
@@ -0,0 +1,6 @@
+namespace wg.modules.tickets.application.Clients.Companies.DTO;
+
+public class CompanySlaTimeDto
+{
+    public TimeSpan SlaTime { get; set; }
+}
---
+++ working-good/src/Modules/Tickets/wg.modules.tickets.application/Clients/Companies/ICompaniesApiClient.cs
@@ -0,0 +1,8 @@
+using wg.modules.tickets.application.Clients.Companies.DTO;
+
+namespace wg.modules.tickets.application.Clients.Companies;
+
+public interface ICompaniesApiClient
+{
+    Task<CompanySlaTimeDto> GetSlaTimeByEmployee(Guid employeeId);
+}
