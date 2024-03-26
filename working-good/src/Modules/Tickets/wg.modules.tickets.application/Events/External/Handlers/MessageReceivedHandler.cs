14ca0abb6a175fca0292184d50597553c59a8216
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-03-26 17:10:12 +0100

    Added event handler for message received event

---
+++ working-good/src/Modules/Tickets/wg.modules.tickets.application/Events/External/Handlers/MessageReceivedHandler.cs
@@ -0,0 +1,14 @@
+using wg.modules.tickets.domain.Services;
+using wg.shared.abstractions.Events;
+
+namespace wg.modules.tickets.application.Events.External.Handlers;
+
+internal sealed class MessageReceivedHandler(
+    INewMessageDomainService newMessageDomainService) : IEventHandler<MessageReceived>
+{
+    public async Task HandleAsync(MessageReceived @event)
+    {
+        await newMessageDomainService.AddNewMessage(Guid.NewGuid(), @event.Sender, @event.Subject,
+            @event.Content, @event.CreatedAt, @event.TicketNumber, null, null);
+    }
+}
