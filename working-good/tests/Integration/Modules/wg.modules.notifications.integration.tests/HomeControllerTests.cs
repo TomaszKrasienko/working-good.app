bc159adfe53b5af817b19e96e77c2baed507997a
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-03-29 10:00:42 +0100

    Added integration tests for notifications HomeController

---
+++ working-good/tests/Integration/Modules/wg.modules.notifications.integration.tests/HomeControllerTests.cs
@@ -0,0 +1,20 @@
+using System.Net;
+using Shouldly;
+using wg.tests.shared.Integration;
+using Xunit;
+
+namespace wg.modules.notifications.integration.tests;
+
+[Collection("#1")]
+public class HomeControllerTests : BaseTestsController
+{
+    [Fact]
+    public async Task Get_ShouldReturn200OkStatusCode()
+    {
+        //act
+        var response = await HttpClient.GetAsync("/messages-module");
+    
+        //assert
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+}
