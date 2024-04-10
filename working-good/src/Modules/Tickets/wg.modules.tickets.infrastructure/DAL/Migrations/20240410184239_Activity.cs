50cc56184b73d34db450d2e5b869f68486b15704
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-04-10 20:42:57 +0200

    Added migrations

---
+++ working-good/src/Modules/Tickets/wg.modules.tickets.infrastructure/DAL/Migrations/20240410184239_Activity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wg.modules.tickets.infrastructure.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Activity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Activity",
+                schema: "tickets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ActivityTimeFrom = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ActivityTimeTo = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsPaid = table.Column<bool>(type: "bit", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TicketId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Activity_Tickets_TicketId",
+                        column: x => x.TicketId,
+                        principalSchema: "tickets",
+                        principalTable: "Tickets",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activity_TicketId",
+                schema: "tickets",
+                table: "Activity",
+                column: "TicketId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Activity",
+                schema: "tickets");
+        }
+    }
+}
