d8b46e9e0c765f6ba26a5f7c0fa41ce87448d92f
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-04-20 17:51:48 +0200

    Fixed vault configuration

--- working-good/src/Shared/wg.shared.infrastructure/Vault/VaultStore.cs
+++ working-good/src/Shared/wg.shared.infrastructure/Vault/VaultStore.cs
@@ -1,19 +1,21 @@
 using Microsoft.Extensions.Options;
 using VaultSharp;
 using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.AuthMethods.UserPass;
 using wg.shared.infrastructure.Vault.Configuration.Models;
 
 namespace wg.shared.infrastructure.Vault;
 
 internal sealed class VaultStore(VaultOptions options)
 {
-    private readonly VaultOptions _options;
-    
+    private readonly VaultOptions _options = options;
     public async Task<IDictionary<string, object>> GetAsync(string key)
     {
-        var settings = new VaultClientSettings(_options.Url, new TokenAuthMethodInfo(_options.Secret));
+        var settings = new VaultClientSettings(options.Url, new TokenAuthMethodInfo(_options.Secret));
+        // var settings =
+        //     new VaultClientSettings(options.Url, new UserPassAuthMethodInfo("kv", "wg", "StrongPassword123"));
         var client = new VaultClient(settings);
-        var secret = await client.V1.Secrets.KeyValue.V2.ReadSecretAsync(key);
+        var secret = await client.V1.Secrets.KeyValue.V2.ReadSecretAsync(key, mountPoint:"kv");
         return secret.Data.Data;
     }
 }
