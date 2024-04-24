2afc9ea236226d6f92fc38b8f127a6e4f1fada3b
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-04-24 20:31:40 +0200

    Added value object for Day. Added directory and project fot activities

--- working-good/src/Modules/Activities/wg.modules.activities.domain/Entities/DailyEmployeeActivities.cs
+++ working-good/src/Modules/Activities/wg.modules.activities.domain/Entities/DailyEmployeeActivities.cs
@@ -1,9 +1,22 @@
+using wg.modules.activities.domain.ValueObjects;
 using wg.shared.abstractions.Kernel.Types;
 
 namespace wg.modules.activities.domain.Entities;
 
 public sealed class DailyEmployeeActivities : AggregateRoot
 {
-    public DateTime Day { get; set; }
-    public EntityId EmployeeId { get; set; }
+    public Day Day { get; private set; }
+    public EntityId EmployeeId { get; private set; }
+    private List<Activity> _activities = new List<Activity>();
+    public IReadOnlyList<Activity> Activities => _activities;
+
+    private DailyEmployeeActivities(AggregateId id, Day day, EntityId employeeId)
+    {
+        Id = id;
+        Day = day;
+        EmployeeId = employeeId;
+    }
+
+    public static DailyEmployeeActivities Create(Guid id, DateTime day, Guid employeeId)
+        => new DailyEmployeeActivities(id, day, employeeId);
 }
---
+++ working-good/src/Modules/Activities/wg.modules.activities.domain/ValueObjects/Day.cs
@@ -0,0 +1,17 @@
+namespace wg.modules.activities.domain.ValueObjects;
+
+public sealed record Day
+{
+    public DateTime Value { get; }
+
+    public Day(DateTime value)
+    {
+        Value = new DateTime(value.Date.Ticks);
+    }
+
+    public static implicit operator DateTime(Day day)
+        => day.Value;
+
+    public static implicit operator Day(DateTime value)
+        => new Day(value);
+}
