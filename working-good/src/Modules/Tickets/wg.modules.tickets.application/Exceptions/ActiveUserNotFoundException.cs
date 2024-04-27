8adb170cf7546b0c1b9ff596e48a114c816e889f
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-04-27 19:52:24 +0200

    Added infrastructure project for activities module

--- working-good/src/Modules/Tickets/wg.modules.tickets.application/Exceptions/ActiveUserNotFoundException.cs
+++
@@ -1,6 +0,0 @@
-using wg.shared.abstractions.Exceptions;
-
-namespace wg.modules.tickets.application.Exceptions;
-
-public sealed class ActiveUserNotFoundException(Guid userId)
-    : WgException($"Active user with ID: {userId} does not exist"); 
