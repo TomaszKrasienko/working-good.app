06c14f60702bb801e435ca5f1aed81e29467523c
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-05-07 21:34:31 +0200

    Refactored JwtOptionsFactory

--- working-good/tests/wg.tests.shared/Factories/Jwt/JwtOptionsFactory.cs
+++ working-good/tests/wg.tests.shared/Factories/Jwt/JwtOptionsFactory.cs
@@ -6,12 +6,12 @@ namespace wg.tests.shared.Factories.Jwt;
 internal static class JwtOptionsFactory
 {
     public static JwtOptions Get()
-    {
-        var faker = new Faker<JwtOptions>()
+        => GetFaker().Generate(1).Single();
+    
+    private static Faker<JwtOptions> GetFaker()
+        => new Faker<JwtOptions>()
             .RuleFor(f => f.Audience, v => v.Lorem.Word())
             .RuleFor(f => f.Issuer, v => v.Lorem.Word())
             .RuleFor(f => f.SigningKey, v => "11DF469417BB6C884189FEAAAFB6A8AB38983A7C34582E77BD816A7AC5")
             .RuleFor(f => f.Expiry, v => TimeSpan.FromHours(2));
-        return faker.Generate(1).Single();
-    }
 }
