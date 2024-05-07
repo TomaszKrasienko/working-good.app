3ce0da3ce4768352ebdac03f93c8dbcf90bbfb16
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-05-07 21:32:53 +0200

    TicketCreatedFactory

--- working-good/tests/wg.tests.shared/Factories/Events/TicketCreatedFactory.cs
+++ working-good/tests/wg.tests.shared/Factories/Events/TicketCreatedFactory.cs
@@ -1,21 +1,24 @@
 using Bogus;
 using wg.modules.notifications.core.Events.External;
+using wg.modules.tickets.domain.Entities;
 
 namespace wg.tests.shared.Factories.Events;
 
 internal static class TicketCreatedFactory
 {
     internal static TicketCreated Get(bool withUser = false, bool withEmployee = false)
-    {
-        var ticketCreatedFaker = new Faker<TicketCreated>()
+        => Get(1, withUser, withEmployee).Single();
+    
+    private static List<TicketCreated> Get(int count, bool withUser, bool withEmployee)
+        => GetFaker(withUser, withEmployee).Generate(1);
+            
+    private static Faker<TicketCreated> GetFaker(bool withUser, bool withEmployee)
+        =>  new Faker<TicketCreated>()
             .CustomInstantiator(f => new TicketCreated(
-                Guid.NewGuid(),
-                f.Random.Number(),
-                f.Lorem.Sentence(4),
-                f.Lorem.Sentence(10),
-                withUser ? Guid.NewGuid() : null,
-                withEmployee ? Guid.NewGuid() : null));
-
-        return ticketCreatedFaker.Generate(1).Single();
-    }
+            Guid.NewGuid(),
+            f.Random.Number(),
+            f.Lorem.Sentence(4),
+            f.Lorem.Sentence(10),
+            withUser ? Guid.NewGuid() : null,
+            withEmployee ? Guid.NewGuid() : null));
 }
