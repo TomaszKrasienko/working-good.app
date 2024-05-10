64a6b5223df088dfc83a5aeeb5db617501e4ebe5
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-05-10 17:26:41 +0200

    Added implementation of get activity be id query handler

--- working-good/src/Modules/Activities/wg.modules.activities.infrastructure/Queries/Handlers/GetActivityByIdQueryHandler.cs
+++ working-good/src/Modules/Activities/wg.modules.activities.infrastructure/Queries/Handlers/GetActivityByIdQueryHandler.cs
@@ -1,6 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 using wg.modules.activities.application.CQRS.Activities.Queries;
 using wg.modules.activities.application.DTOs;
 using wg.modules.activities.infrastructure.DAL;
+using wg.modules.activities.infrastructure.Queries.Mappers;
 using wg.shared.abstractions.CQRS.Queries;
 
 namespace wg.modules.activities.infrastructure.Queries.Handlers;
@@ -8,8 +10,9 @@ namespace wg.modules.activities.infrastructure.Queries.Handlers;
 internal sealed class GetActivityByIdQueryHandler(
     ActivitiesDbContext dbContext) : IQueryHandler<GetActivityById, ActivityDto>
 {
-    public Task<ActivityDto> HandleAsync(GetActivityById query, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<ActivityDto> HandleAsync(GetActivityById query, CancellationToken cancellationToken)
+        => (await dbContext
+                .Activities
+                .FirstOrDefaultAsync(x => x.Id.Equals(query.Id), cancellationToken))?
+            .AsDto();
 }
