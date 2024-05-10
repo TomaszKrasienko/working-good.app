bfe7ff014d68b9f3388117ac9104ff44997e3b1a
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-05-10 17:28:36 +0200

    Added endpoint for getting activity by id

--- working-good/src/Modules/Activities/wg.modules.activities.api/Controllers/ActivitiesController.cs
+++ working-good/src/Modules/Activities/wg.modules.activities.api/Controllers/ActivitiesController.cs
@@ -4,23 +4,25 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using wg.modules.activities.application.CQRS.Activities.Commands.AddActivity;
+using wg.modules.activities.application.CQRS.Activities.Queries;
+using wg.modules.activities.application.DTOs;
 using wg.shared.abstractions.CQRS.Commands;
+using wg.shared.abstractions.CQRS.Queries;
 
 namespace wg.modules.activities.api.Controllers;
 
 internal sealed class ActivitiesController(
-    ICommandDispatcher commandDispatcher) : BaseController
+    ICommandDispatcher commandDispatcher,
+    IQueryDispatcher queryDispatcher) : BaseController
 {
     [HttpGet("{id:guid}")]
     [Authorize]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    public async Task<ActionResult> GetById()
-    {
-        //TODO: to finish
-        return Ok();
-    }
+    [SwaggerOperation(Summary = "Gets activity by identifier")]
+    public async Task<ActionResult<ActivityDto>> GetById(Guid id, CancellationToken cancellationToken)
+        => Ok(await queryDispatcher.SendAsync(new GetActivityById(id), cancellationToken));
     
     [HttpPost("add")]
     [Authorize]
