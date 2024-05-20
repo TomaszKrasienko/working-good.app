971937a63ef7469a12b8ed1a4a39515058175c61
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-05-20 22:31:07 +0200

    Added unit tests or change project command handler

--- working-good/src/Modules/Tickets/wg.modules.tickets.application/CQRS/Tickets/Commands/ChangeProject/ChangeProjectCommandHandler.cs
+++ working-good/src/Modules/Tickets/wg.modules.tickets.application/CQRS/Tickets/Commands/ChangeProject/ChangeProjectCommandHandler.cs
@@ -1,4 +1,9 @@
 using wg.modules.tickets.application.Clients.Companies;
+using wg.modules.tickets.application.Clients.Companies.DTO;
+using wg.modules.tickets.application.Clients.Owner;
+using wg.modules.tickets.application.Clients.Owner.DTO;
+using wg.modules.tickets.application.Exceptions;
+using wg.modules.tickets.domain.Exceptions;
 using wg.modules.tickets.domain.Repositories;
 using wg.shared.abstractions.CQRS.Commands;
 
@@ -6,11 +11,57 @@ namespace wg.modules.tickets.application.CQRS.Tickets.Commands.ChangeProject;
 
 internal sealed class ChangeProjectCommandHandler(
     ITicketRepository ticketRepository,
-    ICompaniesApiClient companiesApiClient) : ICommandHandler<ChangeProjectCommand>
+    ICompaniesApiClient companiesApiClient,
+    IOwnerApiClient ownerApiClient) : ICommandHandler<ChangeProjectCommand>
 {
     public async Task HandleAsync(ChangeProjectCommand command, CancellationToken cancellationToken)
     {
         var ticket = await ticketRepository.GetByIdAsync(command.TicketId);
+        if (ticket is null)
+        {
+            throw new TicketNotFoundException(command.TicketId);
+        }
+
+        var isActiveProjectExists = await companiesApiClient.IsProjectActiveAsync(new ProjectIdDto()
+        {
+            Id = command.ProjectId
+        });
+
+        if (ticket.AssignedUser is not null)
+        {
+            var isUserBelongToGroup = await ownerApiClient.IsMembershipExistsAsync(new GetMembershipDto()
+            {
+                UserId = ticket.AssignedUser,
+                GroupId = command.ProjectId
+            });
+            
+            if (!isUserBelongToGroup.Value)
+            {
+                throw new UserDoesNotBelongToGroupException(command.ProjectId, ticket.AssignedUser);
+            }
+        }
+
+        if (ticket.AssignedEmployee is not null)
+        {
+            var isProjectForEmployee = await companiesApiClient.IsProjectForCompanyAsync(new EmployeeWithProjectDto()
+            {
+                EmployeeId = ticket.AssignedEmployee,
+                ProjectId = command.ProjectId
+            });
+
+            if (!isProjectForEmployee.Value)
+            {
+                throw new InvalidProjectForEmployeeException(ticket.AssignedEmployee, command.ProjectId);
+            }
+        }
+        else
+        {
+            if (!isActiveProjectExists.Value)
+            {
+                throw new ActiveProjectNotFoundException(command.ProjectId);
+            }
+        }
+
         ticket.ChangeProject(command.ProjectId);
         await ticketRepository.UpdateAsync(ticket);
     }
--- working-good/tests/Modules/Tickets/wg.modules.tickets.application.tests/CQRS/Tickets/Commands/ChangeProjectCommandHandlerTest.cs
+++ working-good/tests/Modules/Tickets/wg.modules.tickets.application.tests/CQRS/Tickets/Commands/ChangeProjectCommandHandlerTest.cs
@@ -3,6 +3,8 @@ using NSubstitute;
 using Shouldly;
 using wg.modules.tickets.application.Clients.Companies;
 using wg.modules.tickets.application.Clients.Companies.DTO;
+using wg.modules.tickets.application.Clients.Owner;
+using wg.modules.tickets.application.Clients.Owner.DTO;
 using wg.modules.tickets.application.CQRS.Tickets.Commands.ChangeProject;
 using wg.modules.tickets.application.Exceptions;
 using wg.modules.tickets.domain.Exceptions;
@@ -18,7 +20,53 @@ public sealed class ChangeProjectCommandHandlerTests
     private Task Act(ChangeProjectCommand command) => _handler.HandleAsync(command, default);
 
     [Fact]
-    public async Task HandleAsync_GivenExistingTicketAndNotAssignedEmployeeAndUser_ShouldChangeProject()
+    public async Task HandleAsync_GivenExistingTicketAssignedEmployeeAndUser_ShouldChangeProjectAndUpdateTicket()
+    {
+        //arrange
+        var ticket = TicketsFactory.Get();
+        var projectId = Guid.NewGuid();
+        var employeeId = Guid.NewGuid();
+        var userId = Guid.NewGuid();
+        
+        ticket.ChangeAssignedEmployee(employeeId);
+        ticket.ChangeAssignedUser(userId);
+        
+        var command = new ChangeProjectCommand(ticket.Id, projectId);
+        _ticketRepository
+            .GetByIdAsync(command.TicketId)
+            .Returns(ticket);
+
+        _companiesApiClient
+            .IsProjectForCompanyAsync(Arg.Is<EmployeeWithProjectDto>(arg
+                => arg.EmployeeId == employeeId
+                   && arg.ProjectId == projectId))
+            .Returns(new IsProjectForCompanyDto()
+            {
+                Value = true
+            });
+        
+        _ownerApiClient
+            .IsMembershipExistsAsync(Arg.Is<GetMembershipDto>(arg
+                => arg.GroupId == projectId
+                   && arg.UserId == userId))
+            .Returns(new IsGroupMembershipExists()
+            {
+                Value = true
+            });
+        
+        //act
+        await Act(command);
+        
+        //assert
+        ticket.ProjectId.Value.ShouldBe(projectId);
+
+        await _ticketRepository
+            .Received(1)
+            .UpdateAsync(ticket);
+    }
+
+    [Fact]
+    public async Task HandleAsync_GivenExistingTicketAndNotAssignedEmployeeAndUser_ShouldChangeProjectAndUpdateTicket()
     {
         //arrange
         var ticket = TicketsFactory.Get();
@@ -46,6 +94,66 @@ public sealed class ChangeProjectCommandHandlerTests
         ticket.ProjectId.Value.ShouldBe(command.ProjectId);
     }
 
+        [Fact]
+    public async Task HandleAsync_GivenExistingTicketAndNotExistingUserMembership_ShouldThrowUserDoesNotBelongToGroupException()
+    {
+        //arrange
+        var ticket = TicketsFactory.Get();
+        var projectId = Guid.NewGuid();
+        var userId = Guid.NewGuid();
+        ticket.ChangeAssignedUser(userId);
+        var command = new ChangeProjectCommand(ticket.Id, projectId);
+        
+        _ticketRepository
+            .GetByIdAsync(command.TicketId)
+            .Returns(ticket);
+
+        _ownerApiClient
+            .IsMembershipExistsAsync(Arg.Is<GetMembershipDto>(arg
+                => arg.GroupId == projectId
+                   && arg.UserId == userId))
+            .Returns(new IsGroupMembershipExists()
+            {
+                Value = false
+            });
+        
+        //act
+        var exception = await Record.ExceptionAsync(async () => await Act(command));
+        
+        //assert
+        exception.ShouldBeOfType<UserDoesNotBelongToGroupException>();
+    }
+
+    [Fact]
+    public async Task HandleAsync_GivenExistingTicketAndEmployeeNotInCompanyWithProject_ShouldThrow()
+    {
+        //arrange
+        var ticket = TicketsFactory.Get();
+        var employeeId = Guid.NewGuid();
+        var projectId = Guid.NewGuid();
+        ticket.ChangeAssignedEmployee(employeeId);
+        var command = new ChangeProjectCommand(ticket.Id, projectId);
+        
+        _ticketRepository
+            .GetByIdAsync(command.TicketId)
+            .Returns(ticket);
+
+        _companiesApiClient
+            .IsProjectForCompanyAsync(Arg.Is<EmployeeWithProjectDto>(arg
+                => arg.EmployeeId == employeeId
+                   && arg.ProjectId == projectId))
+            .Returns(new IsProjectForCompanyDto()
+            {
+                Value = false
+            });
+        
+        //act
+        var exception = await Record.ExceptionAsync(async () => await Act(command));
+        
+        //assert
+        exception.ShouldBeOfType<InvalidProjectForEmployeeException>();
+    }
+    
     [Fact]
     public async Task HandleAsync_GivenNotExistingTicket_ShouldThrowTicketNotFoundException()
     {
@@ -87,13 +195,16 @@ public sealed class ChangeProjectCommandHandlerTests
     #region arrage
     private readonly ITicketRepository _ticketRepository;
     private readonly ICompaniesApiClient _companiesApiClient;
+    private readonly IOwnerApiClient _ownerApiClient;
     private readonly ICommandHandler<ChangeProjectCommand> _handler;
 
     public ChangeProjectCommandHandlerTests()
     {
         _ticketRepository = Substitute.For<ITicketRepository>();
         _companiesApiClient = Substitute.For<ICompaniesApiClient>();
-        _handler = new ChangeProjectCommandHandler(_ticketRepository, _companiesApiClient);
+        _ownerApiClient = Substitute.For<IOwnerApiClient>();
+        _handler = new ChangeProjectCommandHandler(_ticketRepository, _companiesApiClient,
+            _ownerApiClient);
     }
     #endregion
 }
