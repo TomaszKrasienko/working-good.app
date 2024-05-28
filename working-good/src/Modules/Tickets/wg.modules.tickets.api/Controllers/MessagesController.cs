a5e2bd693ab867e524baec152fcb5b179eac66ce
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-05-28 18:12:29 +0200

    Uncomment two unit tests for negative path for add message command handler

--- working-good/src/Modules/Tickets/wg.modules.tickets.api/Controllers/MessagesController.cs
+++ working-good/src/Modules/Tickets/wg.modules.tickets.api/Controllers/MessagesController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using wg.modules.tickets.application.CQRS.Messages.Commands.AddMessage;
 using wg.modules.tickets.application.CQRS.Messages.Queries;
 using wg.modules.tickets.application.DTOs;
@@ -20,6 +21,7 @@ internal sealed class MessagesController(
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [SwaggerOperation("Gets message by \"ID\"")]
     public async Task<ActionResult<MessageDto>> GetById(Guid id, CancellationToken cancellationToken)
         => Ok(await queryDispatcher.SendAsync(new GetMessageByIdQuery(id), cancellationToken));
 
@@ -28,6 +30,7 @@ internal sealed class MessagesController(
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [SwaggerOperation("Adds message to ticket")]
     public async Task<ActionResult> AddMessage(AddMessageCommand command, Guid ticketId, CancellationToken cancellationToken)
     {
         var id = Guid.NewGuid();
--- working-good/tests/Modules/Tickets/wg.modules.tickets.application.tests/CQRS/Messages/Commands/AddMessageCommandHandler.cs
+++ working-good/tests/Modules/Tickets/wg.modules.tickets.application.tests/CQRS/Messages/Commands/AddMessageCommandHandler.cs
@@ -1,30 +1,29 @@
-// using NSubstitute;
-// using Shouldly;
-// using wg.modules.owner.domain.ValueObjects.User;
-// using wg.modules.tickets.application.Clients.Owner;
-// using wg.modules.tickets.application.Clients.Owner.DTO;
-// using wg.modules.tickets.application.CQRS.Messages.Commands.AddMessage;
-// using wg.modules.tickets.application.Events;
-// using wg.modules.tickets.application.Exceptions;
-// using wg.modules.tickets.domain.Entities;
-// using wg.modules.tickets.domain.Exceptions;
-// using wg.modules.tickets.domain.Repositories;
-// using wg.shared.abstractions.Messaging;
-// using wg.shared.abstractions.Time;
-// using wg.tests.shared.Factories.Tickets;
-// using wg.tests.shared.Mocks;
-// using Xunit;
-// using State = wg.modules.tickets.domain.ValueObjects.Ticket.State;
-//
-// namespace wg.modules.tickets.application.tests.CQRS.Messages.Commands;
-//
-// public sealed class AddMessageCommandHandlerTests
-// {
-//     private Task Act(AddMessageCommand command) => _handler.HandleAsync(command, default);
-//     
-//     [Fact]
-//     public async Task HandleAsync_GivenExistingTicket_ShouldUpdateTicketWithNewMessageAndSendEvent()
-//     {
+using NSubstitute;
+using Shouldly;
+using wg.modules.owner.domain.ValueObjects.User;
+using wg.modules.tickets.application.Clients.Owner;
+using wg.modules.tickets.application.Clients.Owner.DTO;
+using wg.modules.tickets.application.CQRS.Messages.Commands.AddMessage;
+using wg.modules.tickets.application.Events;
+using wg.modules.tickets.application.Exceptions;
+using wg.modules.tickets.domain.Entities;
+using wg.modules.tickets.domain.Exceptions;
+using wg.modules.tickets.domain.Repositories;
+using wg.shared.abstractions.Messaging;
+using wg.shared.abstractions.Time;
+using wg.tests.shared.Factories.Tickets;
+using wg.tests.shared.Mocks;
+using Xunit;
+
+namespace wg.modules.tickets.application.tests.CQRS.Messages.Commands;
+
+public sealed class AddMessageCommandHandlerTests
+{
+    private Task Act(AddMessageCommand command) => _handler.HandleAsync(command, default);
+    
+    // [Fact]
+    // public async Task HandleAsync_GivenExistingTicket_ShouldUpdateTicketWithNewMessageAndSendEvent()
+    // {
 //         //arrange
 //         var ticket = TicketsFactory.Get();
 //         var command = new AddMessageCommand(Guid.NewGuid(), Guid.NewGuid(), "Test message contet",
@@ -123,55 +122,55 @@
 //                    && arg.EmployeeId == null));
 //     }
 //     
-//     [Fact]
-//     public async Task HandleAsync_GivenNotExistingTicket_ShouldUThrowTicketNotFoundException()
-//     {
-//         //arrange
-//         var command = new AddMessageCommand(Guid.NewGuid(), Guid.NewGuid(), "Test message contet",
-//             Guid.NewGuid());
-//         
-//         //act
-//         var exception = await Record.ExceptionAsync(async () => await Act(command));
-//         
-//         //assert
-//         exception.ShouldBeOfType<TicketNotFoundException>();
-//     }
-//     
-//     [Fact]
-//     public async Task HandleAsync_GivenNotExistingUserDto_ShouldThrowUserNotFoundException()
-//     {
-//         //arrange
-//         var ticket = TicketsFactory.GetOnlyRequired(state: State.Open());
-//         var command = new AddMessageCommand(Guid.NewGuid(), Guid.NewGuid(), "Test message contet",
-//             ticket.Id);
-//         
-//         _ticketRepository
-//             .GetByIdAsync(command.TicketId)
-//             .Returns(ticket);
-//         
-//         //act
-//         var exception = await Record.ExceptionAsync(async() => await Act(command));
-//         
-//         //assert
-//         exception.ShouldBeOfType<UserNotFoundException>();
-//     }
-//
-//     
-//     #region arrange
-//     private readonly ITicketRepository _ticketRepository;
-//     private readonly IOwnerApiClient _ownerApiClient;
-//     private readonly IMessageBroker _messageBroker;
-//     private readonly DateTime _now = DateTime.Now;
-//     private readonly AddMessageCommandHandler _handler;
-//
-//     public AddMessageCommandHandlerTests()
-//     {
-//         _ticketRepository = Substitute.For<ITicketRepository>();
-//         _ownerApiClient = Substitute.For<IOwnerApiClient>();
-//         _messageBroker = Substitute.For<IMessageBroker>();
-//         var clock = TestsClock.Create(_now);
-//         _handler = new AddMessageCommandHandler(_ticketRepository, _ownerApiClient, _messageBroker,
-//             clock);
-//     }
-//     #endregion
-// }
+     [Fact]
+     public async Task HandleAsync_GivenNotExistingTicket_ShouldUThrowTicketNotFoundException()
+     {
+         //arrange
+         var command = new AddMessageCommand(Guid.NewGuid(), Guid.NewGuid(), "Test message contet",
+             Guid.NewGuid());
+         
+         //act
+         var exception = await Record.ExceptionAsync(async () => await Act(command));
+         
+         //assert
+         exception.ShouldBeOfType<TicketNotFoundException>();
+     }
+     
+     [Fact]
+     public async Task HandleAsync_GivenNotExistingActiveUserDto_ShouldThrowActiveUserNotFoundException()
+     {
+         //arrange
+         var ticket = TicketsFactory.Get();
+         var command = new AddMessageCommand(Guid.NewGuid(), Guid.NewGuid(), "Test message content",
+             ticket.Id);
+         
+         _ticketRepository
+             .GetByIdAsync(command.TicketId)
+             .Returns(ticket);
+         
+         //act
+         var exception = await Record.ExceptionAsync(async() => await Act(command));
+         
+         //assert
+         exception.ShouldBeOfType<ActiveUserNotFoundException>();
+     }
+
+    
+    #region arrange
+    private readonly ITicketRepository _ticketRepository;
+    private readonly IOwnerApiClient _ownerApiClient;
+    private readonly IMessageBroker _messageBroker;
+    private readonly DateTime _now = DateTime.Now;
+    private readonly AddMessageCommandHandler _handler;
+
+    public AddMessageCommandHandlerTests()
+    {
+        _ticketRepository = Substitute.For<ITicketRepository>();
+        _ownerApiClient = Substitute.For<IOwnerApiClient>();
+        _messageBroker = Substitute.For<IMessageBroker>();
+        var clock = TestsClock.Create(_now);
+        _handler = new AddMessageCommandHandler(_ticketRepository, _ownerApiClient, _messageBroker,
+            clock);
+    }
+    #endregion
+}
