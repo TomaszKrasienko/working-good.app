32fd02d87de5f618a19b638fe227499c47239411
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-05-31 14:35:45 +0200

    Added unit tests and implementation for Create method in section module

--- working-good/src/Modules/Wiki/wg.modules.wiki.core/Entities/Section.cs
+++ working-good/src/Modules/Wiki/wg.modules.wiki.core/Entities/Section.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using wg.modules.wiki.core.ValueObjects.Section;
 using wg.shared.abstractions.Kernel.Types;
 
@@ -9,10 +10,27 @@ public sealed class Section
     public Name Name { get; private set; }
     public Section Parent { get; private set; }
 
-    private Section(EntityId id, Name name, Section parent)
+    private Section(EntityId id)
     {
         Id = id;
-        Name = name;
-        Parent = parent;
     }
+
+    internal static Section Create(Guid id, string name, Section parent = null)
+    {
+        var section = new Section(id);
+        section.ChangeName(name);
+        if (parent is not null)
+        {
+            section.ChangeParent(parent);
+        }
+
+        return section;
+    }
+
+    private void ChangeName(string name)
+        => Name = name;
+
+    private void ChangeParent(Section parent)
+        => Parent = parent;
+
 }
--- working-good/tests/Modules/Wiki/wg.modules.wiki.core.tests/Entities/Create/SectionTests.cs
+++ working-good/tests/Modules/Wiki/wg.modules.wiki.core.tests/Entities/Create/SectionTests.cs
@@ -1,6 +1,52 @@
+using Shouldly;
+using wg.modules.wiki.core.Entities;
+using wg.modules.wiki.core.Exceptions;
+using Xunit;
+
 namespace wg.modules.wiki.core.tests.Entities.Create;
 
-public class SectionCreateTests
+public sealed class SectionCreateTests
 {
+    [Fact]
+    public void Create_GivenWithParent_ShouldReturnSectionWithParent()
+    {
+        //arrange
+        var parent = Section.Create(Guid.NewGuid(), "Parent section");
+        var name = "Child section name";
+        var id = Guid.NewGuid();
+        
+        //act
+        var result = Section.Create(id, name, parent);
+        
+        //assert
+        result.Id.Value.ShouldBe(id);
+        result.Name.Value.ShouldBe(name);
+        result.Parent.ShouldBe(parent);
+    }
     
+    [Fact]
+    public void Create_GivenWithoutParent_ShouldReturnSectionWithoutParent()
+    {
+        //arrange
+        var name = "Child section name";
+        var id = Guid.NewGuid();
+        
+        //act
+        var result = Section.Create(id, name);
+        
+        //assert
+        result.Id.Value.ShouldBe(id);
+        result.Name.Value.ShouldBe(name);
+        result.Parent.ShouldBeNull();
+    }
+    
+    [Fact]
+    public void Create_GivenEmptyName_ShouldThrowEmptySectionNameException()
+    {
+        //act
+        var exception = Record.Exception(() => Section.Create(Guid.NewGuid(), string.Empty));
+        
+        //assert
+        exception.ShouldBeOfType<EmptySectionNameException>();
+    }
 }
